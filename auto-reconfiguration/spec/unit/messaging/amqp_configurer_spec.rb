2d9ec6b0da079290a678e0f35f2f4326529710a0
Jennifer Hickey <jhickey@vmware.com>
2011-12-01 10:21:30 -0800

    Carrot auto-reconfiguration
    Change-Id: Ia9167faeefa66999554c2325a40923aff1931990

--- auto-reconfiguration/spec/unit/messaging/amqp_configurer_spec.rb
+++ auto-reconfiguration/spec/unit/messaging/amqp_configurer_spec.rb
@@ -12,7 +12,7 @@ describe 'AutoReconfiguration::Rabbit' do
   end
 
   it 'auto-configures AMQP on connect with options and new format (srs)' do
-    conn_error = attempt_connect({:host => '127.0.0.1', :port =>1234, :user=>'testuser',
+    conn_error = attempt_amqp_connect({:host => '127.0.0.1', :port =>1234, :user=>'testuser',
       :pass=>'testpass', :vhost=>'testvhost'})
     conn_error.should =~ /Could not estabilish TCP connection to 10.20.30.40:12345/
   end
@@ -22,13 +22,13 @@ describe 'AutoReconfiguration::Rabbit' do
       '"plan": "free", "credentials": {"hostname": "10.20.30.40", "port": 12345, "user": "username",
       "pass":"password", "vhost" : "virtualHost"}}]}'
     load 'cfruntime/properties.rb'
-    conn_error = attempt_connect ({:host => '127.0.0.1', :port =>1234, :user=>'testuser',
+    conn_error = attempt_amqp_connect ({:host => '127.0.0.1', :port =>1234, :user=>'testuser',
       :pass=>'testpass', :vhost=>'testvhost'})
     conn_error.should =~ /Could not estabilish TCP connection to 10.20.30.40:12345/
   end
 
   it 'auto-configures AMQP on connect with url and new format (srs)' do
-    conn_error = attempt_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
+    conn_error = attempt_amqp_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
     conn_error.should =~ /Could not estabilish TCP connection to 10.20.30.40:12345/
   end
 
@@ -37,14 +37,14 @@ describe 'AutoReconfiguration::Rabbit' do
       '"plan": "free", "credentials": {"hostname": "10.20.30.40", "port": 12345, "user": "username",
       "pass":"password", "vhost" : "virtualHost"}}]}'
     load 'cfruntime/properties.rb'
-    conn_error = attempt_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
+    conn_error = attempt_amqp_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
     conn_error.should =~ /Could not estabilish TCP connection to 10.20.30.40:12345/
   end
 
   it 'does not auto-configure Rabbit if VCAP_SERVICES not set' do
     ENV['VCAP_SERVICES'] = nil
     load 'cfruntime/properties.rb'
-    conn_error = attempt_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
+    conn_error = attempt_amqp_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
     conn_error.should =~ /Could not estabilish TCP connection to 127.0.0.1:1234/
   end
 
@@ -52,21 +52,21 @@ describe 'AutoReconfiguration::Rabbit' do
      load 'cfautoconfig/messaging/amqp_configurer.rb'
      #This would blow up massively (stack trace too deep) if we
      #aliased the connect methods twice
-     conn_error = attempt_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
+     conn_error = attempt_amqp_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
      conn_error.should =~ /Could not estabilish TCP connection to 10.20.30.40:12345/
    end
 
   it 'disables Rabbit auto-reconfig if DISABLE_AUTO_CONFIG includes rabbitmq' do
     ENV['DISABLE_AUTO_CONFIG'] = "redis:rabbitmq:mongodb"
     load 'cfautoconfig/messaging/amqp_configurer.rb'
-    conn_error = attempt_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
+    conn_error = attempt_amqp_connect "amqp://testuser:testpass@127.0.0.1:1234/testvhost"
     conn_error.should =~ /Could not estabilish TCP connection to 127.0.0.1:1234/
   end
 
 end
 
 private
-def attempt_connect(connection_options_or_string)
+def attempt_amqp_connect(connection_options_or_string)
   conn_error = ''
   begin
     EventMachine.run do
