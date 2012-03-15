18cfacbc8e15b69f5cf7bd927004b9525e910cac
Thomas Risberg <trisberg@vmware.com>
2012-03-15 07:11:00 -0400

    Adding coverage reports using simplecov-rcov
    Change-Id: Ibf1f5a3891dcb55f12e386516bf2abade26e3746

--- auto-reconfiguration/spec/spec_helper.rb
+++ auto-reconfiguration/spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start
+
 $:.unshift('./lib')
 require 'bundler'
 require 'bundler/setup'
